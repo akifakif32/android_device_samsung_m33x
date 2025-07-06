322802977ffb92efa7a8d6253fc8b0b5cd218ab1
Flopster101 <nahuelgomez329@gmail.com>
2025-07-06 03:22:27 -0300

    m33x: Patch non-VTS compliant properties in Vaultkeeper
    They have to have vendor prefixes to be compliant.
    
    Change-Id: I5e886341c4bef327547f2fa6ca99ef99cc98e99d
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -35,6 +35,10 @@ lib_fixups: lib_fixups_user_type = {
 }  # fmt: skip
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/vaultkeeperd': blob_fixup()
+        .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
+    'vendor/lib64/libvkservice.so': blob_fixup()
+        .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
     (
         'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
         'vendor/lib64/libskeymint10device.so',
