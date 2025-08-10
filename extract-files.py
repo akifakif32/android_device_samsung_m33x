d6761088a38cce617ac0fcceb022f8b0c750aa04
Majaahh <maja.blaszczak@proton.me>
2025-08-10 17:59:11 +0200

    m33x: Init
    Change-Id: Ibcb7198adc1fd6c7644e22e0189f5170880b6f0d
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ extract-files.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/s5e8825-common',
+    'hardware/samsung',
+    'hardware/samsung_slsi-linaro/exynos',
+    'hardware/samsung_slsi-linaro/graphics',
+    'hardware/samsung_slsi-linaro/interfaces',
+    'vendor/samsung/s5e8825-common',
+]
+
+module = ExtractUtilsModule(
+    'm33x',
+    'samsung',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 's5e8825-common', module.vendor
+    )
+    utils.run()
