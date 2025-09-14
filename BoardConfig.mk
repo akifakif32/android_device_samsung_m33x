9aa2e6807f19f6052200e8b3ce0c91d947c72589
Majaahh <maja.blaszczak@proton.me>
2025-09-15 00:26:09 +0200

    m33x: Cleanup device tree
    Change-Id: I8394def95c64b425baa76591809d82551703158e
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit the proprietary files
+# Inherit proprietary files
 include vendor/samsung/m33x/BoardConfigVendor.mk
 
-# Inherit the board configuration from common tree
+# Inherit board configuration from common tree
 include device/samsung/s5e8825-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/m33x
