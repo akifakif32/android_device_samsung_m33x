a1788791dbfeb55173d5ed543ffd79781306e837
Majaahh <maja.blaszczak@proton.me>
2025-09-14 23:58:07 +0200

    m33x: Initial SM-M336K support
    Change-Id: I05a00c3ebaf3ada4ca9656d4a3137e98b8d04546
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Co-authored-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -53,3 +53,13 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# Placeholders
+EMPTY_PLACEHOLDER := $(DEVICE_PATH)/configs/placeholder
+
+PRODUCT_COPY_FILES += \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/NPU.bin \
+    $(EMPTY_PLACEHOLDER):$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin
