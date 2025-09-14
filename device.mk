b98c8912b6309928d770e00da87ffc80f98d553c
Majaahh <maja.blaszczak@proton.me>
2025-09-14 22:34:15 +0200

    m33x: Move tee blobs to a subdirectory
    Change-Id: I57972fd69e15c75578cf3e327c300270ce76856f
    Co-authored-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ $(call soong_config_set,libfmjni,vendor,slsi)
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/init/init.m33x.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.m33x.rc
+    $(DEVICE_PATH)/configs/init/init.m33x.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.m33x.rc \
+    $(DEVICE_PATH)/configs/init/init.m33x.unify.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.m33x.unify.rc
 
 # Nfc
 PRODUCT_PACKAGES += android.hardware.nfc@1.2-service
