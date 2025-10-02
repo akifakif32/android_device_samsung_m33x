4658732fbbaa016a73d74984e7cc4e6f12c5fe1d
Majaahh <maja.blaszczak@proton.me>
2025-10-02 19:05:10 +0200

    m33x: nfc: Migrate to bp
    Change-Id: If6fc0c3e2050bee73caf41d5cca2723e86f16c1d
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -46,10 +46,11 @@ PRODUCT_COPY_FILES += \
 # Nfc
 PRODUCT_PACKAGES += android.hardware.nfc@1.2-service
 
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(DEVICE_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf \
-    $(DEVICE_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+# Nfc - Configuration
+PRODUCT_PACKAGES += \
+    libnfc-nci.conf \
+    libnfc-nxp.conf \
+    libnfc-nxp_RF.conf
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
